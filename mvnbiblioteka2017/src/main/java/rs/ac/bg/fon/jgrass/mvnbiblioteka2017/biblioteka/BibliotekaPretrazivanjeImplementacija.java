60981ecc211663cd950ffd7f86f565ec22282389
UrosMomcilovic <urosuki968@gmail.com>
2017-06-03 12:05:06 +0200

    Kopirane obicne klase, build-ovan projekat

---
+++ mvnbiblioteka2017/src/main/java/rs/ac/bg/fon/jgrass/mvnbiblioteka2017/biblioteka/BibliotekaPretrazivanjeImplementacija.java
@@ -0,0 +1,23 @@
+package rs.ac.bg.fon.jgrass.mvnbiblioteka2017.biblioteka;
+
+import java.util.LinkedList;
+
+import rs.ac.bg.fon.jgrass.mvnbiblioteka2017.interfejs.BibliotekaPretrazivanje;
+import rs.ac.bg.fon.jgrass.mvnbiblioteka2017.sistemske_operacije.SOPronadjiKnjigu;
+import rs.ac.bg.fon.jgrass.mvnbiblioteka2017.sistemske_operacije.SOUcitajKnjige;
+
+public class BibliotekaPretrazivanjeImplementacija implements BibliotekaPretrazivanje {
+
+	private LinkedList<Knjiga> knjige = 
+			new LinkedList<Knjiga>();
+
+	
+	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, long isbn, String izdavac) {
+		return SOPronadjiKnjigu.izvrsi(naslov, autor, isbn, izdavac, knjige);
+	}
+
+	public void ucitajKnjige(String filepath) throws Exception {
+		knjige = SOUcitajKnjige.izvrsi(filepath);
+	}
+
+}
